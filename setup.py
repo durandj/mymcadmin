7e558c5440126db0960d17a4b778728b393ef6d2
James Durand <james.durand@alumni.msoe.edu>
2016-02-06 09:17:17 -0600

    Update version to 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt', 'r') as req_file:
 
 setuptools.setup(
 	name             = 'mymcadmin',
-	version          = '0.0.0',
+	version          = '0.1.0',
 	packages         = setuptools.find_packages(),
 	install_requires = REQUIREMENTS,
 	entry_points     = """
