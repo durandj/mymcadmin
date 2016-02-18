2ac2fdd7c09bbe95ac16939bb40ebe69bc61692c
James Durand <james.durand@alumni.msoe.edu>
2016-02-17 20:13:21 -0600

    Fixed setup.py not conforming to pylint

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
+"""
+MyMCAdmin installer
+"""
+
 import setuptools
 
 with open('requirements.txt', 'r') as req_file:
-	REQUIREMENTS = req_file.readlines()
+    REQUIREMENTS = req_file.readlines()
 
 setuptools.setup(
-	name             = 'mymcadmin',
-	version          = '0.1.0',
-	packages         = setuptools.find_packages(),
-	install_requires = REQUIREMENTS,
-	entry_points     = """
-		[console_scripts]
-		mymcadmin=mymcadmin.cli:mymcadmin
-	""",
+    name             = 'mymcadmin',
+    version          = '0.1.0',
+    packages         = setuptools.find_packages(),
+    install_requires = REQUIREMENTS,
+    entry_points     = """
+        [console_scripts]
+        mymcadmin=mymcadmin.cli:mymcadmin
+    """,
 )
 
