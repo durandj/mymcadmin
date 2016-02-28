22d92fa5737eba6021da2755f5d2a36d2060dc45
James Durand <james.durand@alumni.msoe.edu>
2016-02-28 13:52:25 -0600

    Added server cards to the dashboard

--- assets/mymcadmin/components/pages/dashboard.jsx
+++ assets/mymcadmin/components/pages/dashboard.jsx
@@ -1,10 +1,60 @@
 import React from 'react';
 
+import ServerCard from '../serverCard';
+
+const dashboardGridStyles = {
+	display:        'flex',
+	flexWrap:       'wrap',
+	justifyContent: 'center'
+};
+
 class Dashboard extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			servers: []
+		};
+
+		// TODO(durandj): remove after debugging
+		function randInt(max, min) {
+			min = min || 0;
+
+			let magnitude = Math.pow(10, Math.floor(Math.log10(max)) + 1);
+			let range     = max - min;
+
+			return Math.floor(Math.random() * magnitude) % range + min;
+		}
+
+		let servers = randInt(5, 15);
+		for (let i = 0; i != servers; i++) {
+			let state = ['Running', 'Stopped'][randInt(2)];
+
+			this.state.servers.push({
+				server_id: 'server_' + i,
+				status:    state,
+				players:   {
+					online: state === 'Running' ? randInt(20) : 0,
+					max:    20
+				}
+			});
+		}
+	}
+
 	render() {
 		return (
-			<span>TODO(durandj): dashboard</span>
+			<div style={dashboardGridStyles}>
+				{this.renderServerCards()}
+			</div>
 		);
+	}
+
+	renderServerCards() {
+		return this.state.servers.map((server) => {
+			return (
+				<ServerCard key={server.server_id} server={server} />
+			);
+		});
 	}
 }
 
---
+++ assets/mymcadmin/components/serverCard.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+
+import Card from 'material-ui/lib/card/card';
+import CardActions from 'material-ui/lib/card/card-actions';
+import CardText from 'material-ui/lib/card/card-text';
+import CardTitle from 'material-ui/lib/card/card-title';
+import FlatButton from 'material-ui/lib/flat-button';
+
+const serverCardStyles = {
+	margin: '8px',
+	minWidth: '225px'
+};
+
+const cardLabelStyles = {
+	display:      'inline-block',
+	paddingRight: '4px',
+	textAlign:    'right',
+	width:        '48px'
+};
+
+class ServerCard extends React.Component {
+	onClickStart() {
+		alert('TODO(durandj): start ' + this.props.server.server_id);
+	}
+
+	onClickStop() {
+		alert('TODO(durandj): stop ' + this.props.server.server_id);
+	}
+
+	onClickRestart() {
+		alert('TODO(durandj): restart ' + this.props.server.server_id);
+	}
+
+	render() {
+		let { server, ...otherProps } = this.props;
+
+		return (
+			<Card style={serverCardStyles}>
+				<CardTitle title={server.server_id} />
+				<CardText>
+					<div>
+						<label style={cardLabelStyles}>
+							Status:
+						</label>
+						<span>{server.status}</span>
+					</div>
+					<div>
+						<label style={cardLabelStyles}>
+							Players:
+						</label>
+						<span>
+							<span>{server.players.online}</span>
+							/
+							<span>{server.players.max}</span>
+						</span>
+					</div>
+				</CardText>
+				<CardActions>
+					{this.renderActionButtons()}
+				</CardActions>
+			</Card>
+		);
+	}
+
+	renderActionButtons() {
+		if (this.props.server.status === 'Running') {
+			return [
+				<FlatButton
+					key="stop"
+					label="Stop"
+					onTouchTap={this.onClickStop.bind(this)} />,
+				<FlatButton
+					key="restart"
+					label="Restart"
+					onTouchTap={this.onClickRestart.bind(this)} />
+			];
+		}
+		else {
+			return (
+				<FlatButton
+					key="start"
+					label="Start"
+					onTouchTap={this.onClickStart.bind(this)} />
+			);
+		}
+	}
+}
+
+ServerCard.propTypes = {
+	server: React.PropTypes.object.isRequired
+}
+
+export default ServerCard;
+
